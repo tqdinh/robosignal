253f8c9b4fd00d151d4fd31b0408740d267b7f13
tqdinh <tqdinhhcmus@gmail.com>
2024-11-17 14:55:42 +0700

    remove Procfile

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
+import ssl
 from flask import Flask, request
 from flask_socketio import SocketIO, emit, join_room
 
+
 app = Flask(__name__)
-app.secret_key = 'random secret key!'
+app.secret_key = "random secret key!"
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 
-@socketio.on('join')
+@socketio.on("join")
 def join(message):
-    username = message['username']
-    room = message['room']
+    username = message["username"]
+    room = message["room"]
     join_room(room)
-    print('RoomEvent: {} has joined the room {}\n'.format(username, room))
-    emit('ready', {username: username}, to=room, skip_sid=request.sid)
+    print("RoomEvent: {} has joined the room {}\n".format(username, room))
+    emit("ready", {username: username}, to=room, skip_sid=request.sid)
 
 
-@socketio.on('data')
+@socketio.on("data")
 def transfer_data(message):
-    username = message['username']
-    room = message['room']
-    data = message['data']
-    print('DataEvent: {} has sent the data:\n {}\n'.format(username, data))
-    emit('data', data, to=room, skip_sid=request.sid)
+    username = message["username"]
+    room = message["room"]
+    data = message["data"]
+    print("DataEvent: {} has sent the data:\n {}\n".format(username, data))
+    emit("data", data, to=room, skip_sid=request.sid)
 
 
 @socketio.on_error_default
@@ -30,5 +32,13 @@ def default_error_handler(e):
     socketio.stop()
 
 
-if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5004)
+if __name__ == "__main__":
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    ssl_context.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
+
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=5004,
+        # ssl_context=ssl_context,
+    )
